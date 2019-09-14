8d89e4ed762b1a7477087e286f72eadf1fcf8901
League Student <league@unknown28f076787fa4.attlocal.net>
2019-09-14 11:23:34 -0700

    nasty surprise

--- src/_01_nasty_surprise/nastySurprise.java
+++ src/_01_nasty_surprise/nastySurprise.java
@@ -1,28 +1,70 @@
 package _01_nasty_surprise;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class nastySurprise {
-public static void main(String[] args) {
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JButton button = new JButton();
-	JButton button2 = new JButton();
-	frame.setVisible(true);
-	frame.setPreferredSize(new Dimension(550, 500));
-	button.setText("Trick");
-	button2.setText("Treat");
-	button.setSize(50, 50);
-	button2.setSize(50, 50);
-	button.addActionListener(null);
-	button2.addActionListener(null);
-	panel.add(button2);
-	panel.add(button);
-	frame.pack();
-    
-}
+public class nastySurprise implements ActionListener {
+	static JFrame frame = new JFrame();
+	static JPanel panel = new JPanel();
+	static JButton button = new JButton();
+	static JButton button2 = new JButton();
+
+	public static void main(String[] args) {
+		nastySurprise u = new nastySurprise();
+		frame.setVisible(true);
+		frame.setPreferredSize(new Dimension(550, 500));
+		button.setText("Trick");
+		button2.setText("Treat");
+		button.setSize(50, 50);
+		button2.setSize(50, 50);
+		u.a();
+		panel.add(button2);
+		panel.add(button);
+		frame.add(panel);
+		frame.pack();
+
+	}
+
+	void a() {
+		button.addActionListener(this);
+		button2.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stubpup
+		if (button == e.getSource()) {
+			showPictureFromTheInternet("pups.jpg");
+
+		}
+		if (button2 == e.getSource()) {
+			showPictureFromTheInternet("oj.jpg");
+		}
+	}
+
+	private void showPictureFromTheInternet(String imageUrl) {
+		try {
+			URL url = new URL(imageUrl);
+			Icon icon = new ImageIcon(url);
+			JLabel imageLabel = new JLabel(icon);
+			JFrame frame = new JFrame();
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
