b7301af7be8c73f5f370e1f795f7684e8831d884
League Student <league@iMac-17.attlocal.net>
2019-10-19 11:17:07 -0700

    Typing

--- src/_05_typing_tutor/TypingtutorRunner.java
+++ src/_05_typing_tutor/TypingtutorRunner.java
@@ -3,5 +3,6 @@ package _05_typing_tutor;
 public class TypingtutorRunner {
 public static void main(String[] args) {
 	typingTutor tt = new typingTutor();
+	tt.makeButtons();
 }
 }
--- src/_05_typing_tutor/typingTutor.java
+++ src/_05_typing_tutor/typingTutor.java
@@ -1,10 +1,54 @@
 package _05_typing_tutor;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class typingTutor {
+public class typingTutor implements KeyListener {
 JFrame frame = new JFrame();
+JLabel label = new JLabel();
+char currentLetter;
+void makeButtons() {
+	frame.setVisible(true);
+	frame.setSize(1000, 500);
+	frame.setTitle("Type or Die!");
+	currentLetter = generateRandomLetter();
+	label.setText(Character.toString(currentLetter));
+	label.setFont(label.getFont().deriveFont(100.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
+	frame.add(label);
+	frame.addKeyListener(this);
+}
+
+
+char generateRandomLetter() {
+    Random r = new Random();
+    return (char) (r.nextInt(26) + 'a');
+}
+
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println(e.getSource());
+}
+
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	currentLetter = generateRandomLetter();
+	label.setText(Character.toString(currentLetter));
+}
 
 }
