b5be75f6c3a20e7da6475b50a1ed3f82f43e7f85
League Student <league@iMac-17.attlocal.net>
2019-11-09 11:30:11 -0800

    Fix typing tutor, started book pf illusions

--- src/_05_typing_tutor/typingTutor.java
+++ src/_05_typing_tutor/typingTutor.java
@@ -1,26 +1,32 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class typingTutor implements KeyListener {
 JFrame frame = new JFrame();
 JLabel label = new JLabel();
+JPanel panel = new JPanel();
 char currentLetter;
 void makeButtons() {
+	frame.add(panel);
 	frame.setVisible(true);
 	frame.setSize(1000, 500);
 	frame.setTitle("Type or Die!");
 	currentLetter = generateRandomLetter();
+	panel.add(label);
 	label.setText(Character.toString(currentLetter));
 	label.setFont(label.getFont().deriveFont(100.0f));
 	label.setHorizontalAlignment(JLabel.CENTER);
 	frame.add(label);
 	frame.addKeyListener(this);
+	
 }
 
 
@@ -40,7 +46,14 @@ public void keyTyped(KeyEvent e) {
 @Override
 public void keyPressed(KeyEvent e) {
 	// TODO Auto-generated method stub
+	
 	System.out.println(e.getSource());
+	if(e.getKeyChar() == currentLetter) {
+		System.out.println("Correct!");
+		panel.setBackground(Color.green);
+	}
+	else
+	{panel.setBackground(Color.RED);}
 }
 
 
