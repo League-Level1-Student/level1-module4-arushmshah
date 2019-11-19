416a203990056e129ed2b10f5b672ea8262c11f7
League Student <league@iMac-24.attlocal.net>
2019-11-18 19:52:50 -0800

    Fortune Teler completed!! :poop:

--- src/_07_fortune_teller/Magic8Ball.java
+++ src/_07_fortune_teller/Magic8Ball.java
@@ -7,30 +7,48 @@ package _07_fortune_teller;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+Random r = new Random();
+int rr = new Random().nextInt(4);
 
 	// 3. Print out this variable
+System.out.println(r);
 
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showConfirmDialog(null, "Ask a question for 8 ballz(WOW) plz");
 	// 5. If the random number is 0
+if(rr==0) {
+	
 
 	// -- tell the user "Yes"
-
+	JOptionPane.showConfirmDialog(null, "Yes");
+}
 	// 6. If the random number is 1
+if(rr==1) {
+	
 
 	// -- tell the user "No"
-
+	JOptionPane.showConfirmDialog(null,"No");
+}
 	// 7. If the random number is 2
+if(rr==2) {
+	
 
 	// -- tell the user "Maybe you should ask Google?"
-
+	JOptionPane.showConfirmDialog(null,"Maybe you should ask Google");
+}
 	// 8. If the random number is 3
-
+if(rr==3) {
 	// -- write your own answer
-
+	JOptionPane.showConfirmDialog(null,"They said ask ur own answer, soooo.....");
+}
+}
 }
